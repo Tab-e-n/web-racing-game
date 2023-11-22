7fcbf18241459f86cc858b38bb9ea540baa33901
janhulka <st024713@student.cichnovabrno.cz>
2023-11-22 20:10:09 +0100

    car_data a vote

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -1,9 +1,11 @@
 extends Camera2D
 
 var countdown_textures = [load("res://Textures/ui/countdown/0.png"),
-	load("res://Textures/ui/countdown/1.png"),
-	load("res://Textures/ui/countdown/2.png"),
-	load("res://Textures/ui/countdown/3.png")]
+						load("res://Textures/ui/countdown/1.png"),
+						load("res://Textures/ui/countdown/2.png"),
+						load("res://Textures/ui/countdown/3.png")]
+	
+var vote_buttons;
 
 var fadeout_speed_countdown : float = 0.005
 var fadeout_speed_finish : float = 0.01
@@ -12,12 +14,18 @@ var reset_fadeout = func(): fadeout_buffer = 0
 
 var race_finished : bool = false
 
+var voting : bool = false
+
 var player_count_buffer = 0
 
 
 
 func _ready():
 	$on_finish_image.modulate = Color(0, 0, 0, 0)
+	vote_buttons = [$vote_buttons/b1, $vote_buttons/b2, $vote_buttons/b3, $vote_buttons/b4]
+	
+	for i in range(len(vote_buttons)):
+		vote_buttons[i].connect("button_down", vote_button_pressed.bind([i]))
 	
 
 func _physics_process(_delta):
@@ -48,6 +56,10 @@ func _physics_process(_delta):
 		$countdown.modulate = Color(0, 0, 0, 1)
 		$countdown.texture = countdown_textures[int((get_parent() as Gameplay).countdown) + 1]
 		
+		if voting:
+			$vote_buttons.visible = false
+			voting = false
+		
 	elif $countdown.modulate.a > 0:
 		$countdown.texture = countdown_textures[0]
 		fade_out($countdown, fadeout_speed_countdown)
@@ -65,12 +77,20 @@ func _physics_process(_delta):
 	#player list
 	if Input.is_key_pressed(KEY_TAB):
 		$player_list.visible = true
-	else:
+	elif $player_list.visible == true:
 		$player_list.visible = false
 	
 	if(player_count_buffer != len(Net.players)):
 		update_player_list()
 	player_count_buffer = len(Net.players)
+	
+	#car stats
+	$"car_stats/gear".text = "gear: " + str($"../Racecar".gear)
+	$"car_stats/speed".text = "speed: " + str(int($"../Racecar".curr_speed))
+
+
+func vote_button_pressed(args):
+	Net.vote_map(args[0])
 
 
 func update_player_list():
@@ -83,7 +103,7 @@ func update_player_list():
 func fade_out(node, fadeout_speed):
 	fadeout_buffer += fadeout_speed
 	node.modulate = node.modulate.lerp(Color(0, 0, 0, 0), fadeout_buffer)
-	
+
 
 func format_time(time):
 	var format_zeros = func(x): return "0%d" % x if x / 10 < 1 else "%d" % x
@@ -93,6 +113,14 @@ func format_time(time):
 	text += ":" + format_zeros.call(int(int((get_parent() as Gameplay).race_timer) % 60)) #sekundy
 	text += ":" + format_zeros.call(int(int((get_parent() as Gameplay).race_timer * 60) % 60)) #milisekundy
 	return text
+	
+
+func start_vote():
+	$vote_buttons.visible = true
+	voting = true
+	
+	for i in range(len(vote_buttons)):
+		vote_buttons[i].text = Net.vote_options[i]
 
 
 func _on_gameplay_race_finished(final_time):
