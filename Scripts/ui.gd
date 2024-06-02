58b8a6de1a0da49644a8a3c43ac77ad3eab4b3db
Tab-e-n <howdoesonename@gmail.com>
2024-06-02 22:43:05 +0200

    The polish, aka controls explained ingame

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -5,13 +5,22 @@ extends Control
 var palette_expanded : bool = false
 
 
+@onready var palette_expanded_node : Control = $main/palette_expanded
+@onready var palette_small_node : Control = $main/palette_small
+
+@onready var ip_input : LineEdit = $main/ip_line
+@onready var name_input : LineEdit = $main/name_line
+
+
 func _ready():
+	$main.visible = true
+	$controls.visible = false
 	Net.reset()
 
 
 func _physics_process(_delta):
-	$palette_expanded.visible = palette_expanded
-	$palette_small.visible = not palette_expanded
+	palette_expanded_node.visible = palette_expanded
+	palette_small_node.visible = not palette_expanded
 
 
 func _on_create_button_pressed():
@@ -22,15 +31,15 @@ func _on_create_button_pressed():
 
 
 func _on_join_button_pressed():
-	if Net.join_game($ip_line.text):
+	if Net.join_game(ip_input.text):
 		Net.reset()
 	else:
 		load_game()
 
 
 func load_game():
-	if($name_line.text != ""):
-		Net.player_info["name"] = $name_line.text
+	if(name_input.text != ""):
+		Net.player_info["name"] = name_input.text
 	else:
 		Net.player_info["name"] = "Guest"
 	get_tree().change_scene_to_file("res://gameplay.tscn")
@@ -38,5 +47,10 @@ func load_game():
 
 func _on_car_button_pressed():
 	palette_expanded = not palette_expanded
-	$palette_expanded.update()
-	$palette_small.update()
+	palette_expanded_node.update()
+	palette_small_node.update()
+
+
+func _on_controls_pressed():
+	$main.visible = not $main.visible
+	$controls.visible = not $controls.visible
